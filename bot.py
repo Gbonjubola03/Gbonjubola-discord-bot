62e7223012e5147f9b3fa63e7cc5907f728107ed
Gbonjubola03 <148791178+Gbonjubola03@users.noreply.github.com>
2026-05-09 15:56:10 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="g!", intents=intents)
     
 # Bot Ready
 @bot.event
